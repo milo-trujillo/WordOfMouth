fa4db5b67032d2bc210b7938d6290c28df777bff
Milo Trujillo <milo.trujillo@sonomaacademy.org>
2014-11-15 00:09:18 -0500

    Added networking skeleton files

---
+++ networking.cpp
@@ -0,0 +1,13 @@
+#include <sys/socket.h>  
+#include <netinet/in.h>  
+#include <stdio.h>		// We probably don't need this in C++
+#include <string.h> 	// Probably don't need this either
+#include <stdlib.h>  
+#include <arpa/inet.h> 
+#include <unistd.h> 
+
+// Example networking code found on https://stackoverflow.com/a/9204831
+
+#include "networking.h"
+
+
---
+++ networking.h
@@ -0,0 +1,12 @@
+#ifndef NETWORKING
+#define NETWORKING
+
+// Functions here are meant to be accessed from the main interlink code only
+
+// bool startRelaying();
+
+// bool sendMessage(string msg);
+
+// bool initNetworking(string ip, int port);
+
+#endif
