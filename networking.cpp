7c36104576de98ebe377f96c7eb57a92530815ec
Milo Trujillo <milo.trujillo@sonomaacademy.org>
2014-11-15 18:19:16 -0500

    Client connections get their own threads

--- networking.cpp
+++ networking.cpp
@@ -6,57 +6,22 @@
 #include <arpa/inet.h> 
 #include <unistd.h> 
 
+#include <pthread.h> // Needed for multithreading
+
 // Needed for debugging
-#include <iostream>
+#include <stdio.h>
 
 // Example networking code found on https://stackoverflow.com/a/9204831
 
 // Internal header files go here
 #include "networking.h"
 
-using namespace std;
-
 const int BUFFER_SIZE = 100;
+const int LISTEN_PORT = 7777;
 
-bool relayMessages() 
-{  
-    int sock_desc = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock_desc == -1)
-    {
-        cout << "cannot create socket!" << endl;
-        return false;
-    }
-
-    struct sockaddr_in server;  
-    memset(&server, 0, sizeof(server));  
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;  
-    server.sin_port = htons(7777);  
-    if (bind(sock_desc, (struct sockaddr*)&server, sizeof(server)) != 0)
-    {
-        cout << "cannot bind socket!" << endl;
-        close(sock_desc);  
-        return false;
-    }
-
-    if (listen(sock_desc, 20) != 0)
-    {
-        cout << "cannot listen on socket!" << endl;
-        close(sock_desc);  
-        return false;
-    }
-
-    struct sockaddr_in client;  
-    memset(&client, 0, sizeof(client));  
-    socklen_t len = sizeof(client); 
-    int temp_sock_desc = accept(sock_desc, (struct sockaddr*)&client, &len);  
-    if (temp_sock_desc == -1)
-    {
-        cout << "cannot accept client!" << endl;
-        close(sock_desc);  
-        return false;
-    }
-
+void* handleMessage(void* arg)
+{
+	long sock_desc = (long)arg;
     char buf[BUFFER_SIZE];  
     int k;  
 
@@ -65,30 +30,78 @@ bool relayMessages()
 
     while(true) 
     {      
-        k = recv(temp_sock_desc, buf, 100, 0);      
+        k = recv(sock_desc, buf, BUFFER_SIZE, 0);      
         if (k == -1)
         {
-            cout << endl << "cannot read from client!" << endl;
+            printf("\ncannot read from client!\n");
             break;
         }
         if (k == 0)
         {
-            cout << endl << "client disconnected." << endl;
+            printf("\nclient disconnected.\n");
             break;
         }
         if (k > 0)          
 		{
-            //printf("%*.*s", k, k, buf);  
-			cout << buf;
+            printf("%*.*s", k, k, buf);  
+			//cout << buf;
 		}
         if (strcmp(buf, "exit") == 0)         
             break;      
     }
 
-    close(temp_sock_desc);  
     close(sock_desc);  
+	return NULL;
+}
 
-    cout << "server disconnected" << endl;
+bool relayMessages() 
+{  
+    int sock_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock_desc == -1)
+    {
+        printf("cannot create socket!\n");
+        return false;
+    }
+
+    struct sockaddr_in server;  
+    memset(&server, 0, sizeof(server));  
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;  
+    server.sin_port = htons(LISTEN_PORT);  
+    if (bind(sock_desc, (struct sockaddr*)&server, sizeof(server)) != 0)
+    {
+        printf("cannot bind socket!\n");;
+        close(sock_desc);  
+        return false;
+    }
+
+    if (listen(sock_desc, 20) != 0)
+    {
+        printf("cannot listen on socket!\n");
+        close(sock_desc);  
+        return false;
+    }
+
+	while(true)
+	{
+		struct sockaddr_in client;  
+		memset(&client, 0, sizeof(client));  
+		socklen_t len = sizeof(client); 
+		long client_sock_desc = accept(sock_desc, (struct sockaddr*)&client, &len); 
+		if (client_sock_desc == -1)
+		{
+			printf("cannot accept client!\n");
+			close(sock_desc);  
+			return false;
+		}
+
+		pthread_t clientThread;
+		pthread_create(&clientThread, NULL, handleMessage, (void*)client_sock_desc);
+		//handleMessage((void*)client_sock_desc);
+	}
+
+    close(sock_desc);  
+    printf("server disconnected\n");
     return true;  
 } 
 
