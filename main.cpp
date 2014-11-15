8a2d8bd6b983dffc92e2b11c35c506cb971cc461
Milo Trujillo <milo.trujillo@sonomaacademy.org>
2014-11-15 16:16:30 -0500

    Main code calls networking code now

--- main.cpp
+++ main.cpp
@@ -3,7 +3,21 @@
 	little work itself.
 */
 
+#include <pthread.h> // Needed for multithreading stuff
+#include <iostream>
+
+#include "networking.h"
+
+using namespace std;
+
 int main()
 {
+	pthread_t relayThread;
+	pthread_create(&relayThread, NULL, startRelaying, NULL);
+	cout << "Relaying started." << endl;
+	while(true)
+	{
+		// Other people's code goes here
+	}
 	return 0;
 }
