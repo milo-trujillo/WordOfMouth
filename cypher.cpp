903c891663880149d48a5121909b023abe3ecb99
milo-trujillo <milo.trujillo@sonomaacademy.org>
2014-12-17 21:15:45 -0800

    Added documentation

--- cypher.cpp
+++ cypher.cpp
@@ -3,17 +3,26 @@
 #include <string.h>
 #include <openssl/aes.h>
 
-#include "hash.h"
+#include "hash.h" // For generating digests
 
 using namespace std;
 
+// The salt needs to be consistent across all linked copies of the
+// program, but doesn't need to be cryptographically secure, so I've
+// just hardcoded a random value in here.
 static const unsigned char salt[AES_BLOCK_SIZE] = 
 	{ 0xB6, 0x2C, 0x1B, 0x56, 0x2B, 0xD1, 0x74, 0x93, 0xC2, 0xBF,
 	  0x41, 0x04, 0x33, 0xED, 0xDC, 0x74 };
 static const int KEYLENGTH = 192;
 
+/*
+	Shout out to the example code over here:
+	http://stackoverflow.com/questions/18152913/aes-aes-cbc-128-aes-cbc-192-aes-cbc-256-encryption-decryption-with-openssl-c
+*/
+
 // a simple hex-print routine. could be modified to print 16 bytes-per-line
 // This is for debugging only
+/*
 static void hex_print(const void* pv, size_t len)
 {
 	const unsigned char * p = (const unsigned char*)pv;
@@ -27,6 +36,7 @@ static void hex_print(const void* pv, size_t len)
 	}
 	printf("\n");
 }
+*/
 
 string decypher(const string &msg, const string &key)
 {
@@ -75,25 +85,12 @@ string cypher(const string &msg, const string &key)
 	AES_set_encrypt_key(aes_key, KEYLENGTH, &enc_key);
 	AES_cbc_encrypt(aes_input, aes_output, inputslength, &enc_key, iv_enc, AES_ENCRYPT);
 
+	// Remember that an unsigned char array is *not* a string,
+	// there's no null terminator, so we need to copy it over byte
+	// by byte
 	string result;
 	for( int i = 0; i < outputslength; i++ )
 		result.push_back((char)aes_output[i]);
 	delete aes_key;
 	return result;
 }
-
-/*
-int main(int argc, char **argv)
-{
-	string key = "Password";
-	string orig = "This is a longer and more thorough test";
-	string cyphered = cypher(orig, key);
-	string done = decypher(cyphered, key);
-	cout << "Orig: " << orig << endl;
-	cout << "Done: " << done << endl;
-	if( orig == done && orig != cyphered )
-		cout << "Cyphers operating correctly" << endl;
-	printf("Cypher: %s\n", cyphered.c_str());
-	return 0;
-}
-*/
