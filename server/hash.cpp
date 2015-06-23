d6a2c2484faae334ffb664e639483e09216d975c
Milo Trujillo <milo.trujillo@zdtunited.com>
2015-06-23 07:16:10 +0000

    Added hashing

--- server/hash.cpp
+++ server/hash.cpp
@@ -1,6 +1,27 @@
-#include <nettle/sha3.h>
+#include <nettle/sha2.h>
 
-bool hash(const char* src, char** dst)
+// This is just shorthand so users don't need to know the hash type or
+// implementation we're using.
+int getHashLength()
 {
-	return false;
+	return SHA512_DIGEST_SIZE;
+}
+
+// At the moment nothing can go wrong, and this always returns true. However,
+// if we some day decide to be clever and check for out of memory then the
+// infrastucture for error reporting will already be here.
+bool hash(const char* src, const int len, char** dst)
+{
+	sha512_ctx state;
+	char* digest = new char[SHA512_DIGEST_SIZE];
+
+	// Note: when updating the hash, length refers to the length of input,
+	// but in digest it refers to the length of the destination buffer.
+	sha512_init(&state);
+	sha512_update(&state, len, (const uint8_t*) src);
+	sha512_digest(&state, SHA512_DIGEST_SIZE, (uint8_t*) digest);
+
+	*dst = digest;
+
+	return true;
 }
--- server/hash.h
+++ server/hash.h
@@ -1,10 +1,21 @@
 #ifndef HASH_H
 #define HASH_H
 
-// This function reads the data in src, generates a hash for it, and saves
-// that hash in a string we aim *dst at. Returns success or failure.
-// On failure we guarantee dst will be unallocated.
+/*
+	Note: hash() takes a pointer to an unallocated char* so that it can reserve
+	space for the appropriate hash type.
 
-bool hash(const char* src, char** dst);
+	It returns a boolean for success or failure. On success, *dst is guaranteed
+	to be a string of length getHashLength() containing the binary hash of the
+	data. On failure it is guaranteed that dst will not be allocated.
+
+	Since we can hash binary data as well as ascii we cannot trust strlen(),
+	and must pass the source length explicitly.
+	
+*/
+
+int getHashLength();
+
+bool hash(const char* src, const int len, char** dst);
 
 #endif
