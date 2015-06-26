f2a3e81f861b9d887ffbf9f83c36cc7eae1538ef
Milo Trujillo <milo.trujillo@sonomaacademy.org>
2015-06-26 11:14:31 -0700

    Tests unneeded in main now that we have test harness

--- server/main.cpp
+++ server/main.cpp
@@ -8,7 +8,6 @@
 #include "log.h"
 #include "daemonize.h"
 #include "message.h"
-#include "encode.h" // For debugging
 
 using namespace std;
 
@@ -23,20 +22,7 @@ int main(int argc, char** argv)
 	if( startLogging("/tmp/wordofmouth-log.txt") == false )
 		return 1;
 
-	// Debugging message ascii encoding / decoding
-	Message m("Plaintext demo message");
-	log(LOG_INFO, "Startup successful.");
-	log(LOG_DEBUG, "Attempting a dump of message contents...");
-	string aa = m.getAsciiArmor();
-	log(LOG_DEBUG, aa);
-	log(LOG_DEBUG, "Attempting to decode message contents...");
-	int len;
-	char* decoded;
-	bool success = asciiDecode(aa.c_str(), &len, &decoded);
-	if( success )
-		log(LOG_DEBUG, string(decoded));
-	else
-		log(LOG_DEBUG, "ERROR DECODING");
+	log(LOG_INFO, "Done with all tasks, exiting...");
 
 	return 0;
 }
