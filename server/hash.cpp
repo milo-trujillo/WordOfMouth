b989b5eaa022545df66e2a329f10c561f6ac41a4
Milo Trujillo <milo.trujillo@zdtunited.com>
2015-07-10 09:10:49 +0000

    Added initial HMAC code

--- server/hash.cpp
+++ server/hash.cpp
@@ -1,4 +1,6 @@
 #include <nettle/sha2.h>
+#include <nettle/sha1.h>
+#include <nettle/hmac.h>
 #include <stdlib.h> // For size_t
 
 // This is just shorthand so users don't need to know the hash type or
@@ -23,6 +25,26 @@ bool hash(const char* src, const size_t len, char** dst)
 	sha512_digest(&state, SHA512_DIGEST_SIZE, (uint8_t*) digest);
 
 	*dst = digest;
+
+	return true;
+}
+
+// While SHA1 has known vulnerabilities, HMACs with SHA1 do not. Since there
+// is no difference in security and SHA1 is quite a bit faster than SHA512,
+// we use SHA1 for our HMACs.
+bool hmacGen(
+	const char* src, const size_t slen, 
+	const char* key, const size_t klen,
+	char** dst, size_t* dlen)
+{
+	*dlen = SHA1_DIGEST_SIZE;
+	*dst = new char[SHA1_DIGEST_SIZE];
+
+	// TODO: Look into purging memory, do we need to delete the ctx safely?
+	hmac_sha1_ctx state;
+	hmac_sha1_set_key(&state, klen, (const uint8_t*) key);
+	hmac_sha1_update(&state, slen, (const uint8_t*) src);
+	hmac_sha1_digest(&state, SHA1_DIGEST_SIZE, (uint8_t*) *dst);
 
 	return true;
 }
--- server/hash.h
+++ server/hash.h
@@ -18,4 +18,15 @@ int getHashLength();
 
 bool hash(const char* src, const size_t len, char** dst);
 
+/*
+	hmacGen takes a cyphered message and the key used to cypher it (along with
+	their lengths), and generates a Message Authentication Code, stored in dst,
+	along with its length, stored in dlen.
+*/
+
+bool hmacGen(
+	const char* src, const size_t slen, 
+	const char* key, const size_t klen,
+	char** dst, size_t* dlen);
+
 #endif
